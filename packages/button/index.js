91edb3b3cfd86bfe080c5a4612c2602d7840e65a
winwinds <562258693@qq.com>
2020-02-28 03:44:19 +0800

    feature: 封装button组件（掘金文章举例）

---
+++ packages/button/index.js
@@ -0,0 +1,7 @@
+import AowuButton from './src/button.vue'
+
+AowuButton.install = function (Vue) {
+  Vue.component(AowuButton.name, AowuButton)
+}
+
+export default AowuButton
---
+++ packages/button/src/button.vue
@@ -0,0 +1,11 @@
+<template>
+  <button>auwo-button</button>
+</template>
+<script type="text/babel">
+export default {
+  name: "AowuButton",
+  data() {
+    return {};
+  }
+};
+</script>
